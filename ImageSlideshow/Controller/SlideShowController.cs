ab60cf30ea6bbb29de0064b803085104b59d0248
Dave Schmittwilken <david.schmittwilken@smail.inf.h-brs.de>
2017-02-11 17:30:28 +0100

    implemente image changer

---
+++ ImageSlideshow/Controller/SlideShowController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSlideshow.Controller
+{
+    public class SlideShowController : INotifyPropertyChanged
+    {
+        private string imagePath = @"D:\Projekte\karsten\ImageSlideshow\ImageSlideshow\Resources\sloth1.jpg";
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set
+            {
+                imagePath = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged("ImagePath");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public void ChangeImage()
+        {
+            if(ImagePath.Equals(@"D:\Projekte\karsten\ImageSlideshow\ImageSlideshow\Resources\sloth1.jpg"))
+            {
+                ImagePath = @"D:\Projekte\karsten\ImageSlideshow\ImageSlideshow\Resources\sloth2.jpg";
+            }
+            else
+            {
+                ImagePath = @"D:\Projekte\karsten\ImageSlideshow\ImageSlideshow\Resources\sloth1.jpg";
+            }
+        }
+    }
+}
